cbb15ad1f96ce0d0b8818c88bfee2e42e46f9ca5
Jake Gealer <jake@gealer.email>
2019-10-15 11:47:47 +0100

    fix undefined detection

--- src/kubernetes-tool/templates/properties.vue
+++ src/kubernetes-tool/templates/properties.vue
@@ -16,12 +16,12 @@ limitations under the License.
 
 <template>
     <div :style="{paddingLeft: `${$props.padding}px`}">
-        <div v-for="v in arr" :key="v[0]">
+        <div v-if="!arr">
+            <p>This file could not be parsed.</p>
+        </div>
+        <div v-else v-for="v in arr" :key="v[0]">
             <span v-if="typeof v[1] === 'string'">
                 <p><code class="slim">{{ v[0] }}</code>: {{ v[1] }}</p>
-            </span>
-            <span v-else-if="!v[1]">
-                <p>This file cannot be parsed.</p>
             </span>
             <span v-else>
                 <Properties :padding="padding + 30" :arr="v[1]" />
