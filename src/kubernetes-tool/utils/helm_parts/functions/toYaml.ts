853ff45d04cd4d6e53dc679fde6497fc0edbe89d
Jake Gealer <jake@gealer.email>
2019-10-29 12:07:03 +0000

    better YAML parser

--- src/kubernetes-tool/utils/helm_parts/functions/toYaml.ts
+++ src/kubernetes-tool/utils/helm_parts/functions/toYaml.ts
@@ -2,4 +2,20 @@ import DocumentParser from "../document_parser"
 import { Quote } from "../utils"
 import { safeDump } from "js-yaml"
 
-export default (parser: DocumentParser, args: (string | Quote)[]): string => safeDump(parser.processArg(args[0]))
+export default (parser: DocumentParser, args: (string | Quote)[]): string => {
+    const a = parser.processArg(args[0])
+    if (typeof a === "string") return safeDump(a)
+    if (typeof a === "boolean") return String(a)
+    if (!a) return "null"
+    let d = ""
+    if (Array.isArray(a)) {
+        for (const x of a) {
+            d += `- ${safeDump(x)}\n`
+        }
+        return d
+    }
+    for (const x in a) {
+        d += `${safeDump(x)}: ${safeDump(a[x])}\n`
+    }
+    return d
+}
