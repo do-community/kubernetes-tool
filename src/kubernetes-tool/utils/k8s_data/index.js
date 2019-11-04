7b7f8f2b941c615a2d7c6d70c168e90b34c64531
Jake Gealer <jake@gealer.email>
2019-11-04 15:59:45 +0000

    Add statefulset

--- src/kubernetes-tool/utils/k8s_data/index.js
+++ src/kubernetes-tool/utils/k8s_data/index.js
@@ -1,5 +1,6 @@
 import base from "./base.yml"
 import metadata from "./metadata.json"
 import Service from "./Service.yml"
+import StatefulSet from "./StatefulSet.yml"
 
-export default { base, metadata, Service }
+export default { base, metadata, Service, StatefulSet }
