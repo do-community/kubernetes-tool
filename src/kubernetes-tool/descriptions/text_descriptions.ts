868da758eec2670b731a3fed14376a53a6cf042e
Jake Gealer <jake@gealer.email>
2019-11-05 14:08:38 +0000

    Add text description

--- src/kubernetes-tool/descriptions/text_descriptions.ts
+++ src/kubernetes-tool/descriptions/text_descriptions.ts
@@ -1,3 +1,6 @@
 export default {
-    Service: "description here",
+    Service: "A service is used to expose a set of pods in such a way that they can be easily balanced.",
+    StatefulSet: "A stateful set is used to allow for the graceful modification of pods (up to the replica count) by updating them based on the position of the pod in the set.",
+    Certificate: "This is a certificate which is used to link a HTTPS certificate to a domain.",
+    Deployment: "This is a standard deployment which has no special rules when it comes to gracefully updating.",
 } as Record<string, string>
