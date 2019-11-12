6cd0e724dcac9fe6ae0b1c16bcce3a4df91143a2
Jake Gealer <jake@gealer.email>
2019-11-12 12:51:07 +0000

    Move how this works

--- src/kubernetes-tool/utils/compatibility_test.ts
+++ src/kubernetes-tool/utils/compatibility_test.ts
@@ -2,29 +2,27 @@ import { fs } from "../utils/helm_parts/utils"
 // @ts-ignore
 import { HelmCoreParser } from "./helm"
 
-class CompatibiltiyTest {
-    // Runs the compatibility test.
-    public async test() {
-        let errors = ""
-        let passed = ""
-        let total = 0
-        let works = 0
-        const promises = []
-        for (const item of await fs.ls("stable")) {
-            if (!item.file) {
-                total++
-                promises.push(new HelmCoreParser({}, item.path).promise.then(() => {
-                    passed += item.path + "\n"
-                    works++
-                }).catch(err => {
-                    errors += `${item.path}: ${err}\n`
-                }))
-            }
+// Runs the compatibility test.
+async function test() {
+    let errors = ""
+    let passed = ""
+    let total = 0
+    let works = 0
+    const promises = []
+    for (const item of await fs.ls("stable")) {
+        if (!item.file) {
+            total++
+            promises.push(new HelmCoreParser({}, item.path).promise.then(() => {
+                passed += item.path + "\n"
+                works++
+            }).catch(err => {
+                errors += `${item.path}: ${err}\n`
+            }))
         }
-        await Promise.all(promises)
-        console.log(`${works}/${total}\n---\nFailed\n---\n${errors}\nPassed\n---\n${passed}`)
     }
+    await Promise.all(promises)
+    console.log(`${works}/${total}\n---\nFailed\n---\n${errors}\nPassed\n---\n${passed}`)
 }
 
 // @ts-ignore
-window.compatibiltiyTest = new CompatibiltiyTest().test
+window.compatibiltiyTest = test
