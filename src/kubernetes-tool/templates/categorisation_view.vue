e25ac80441189ce15aa1375ae6eab11e54d0017e
Jake Gealer <jake@gealer.email>
2020-01-14 13:55:09 +0000

    Make collapsing clearer

--- src/kubernetes-tool/templates/categorisation_view.vue
+++ src/kubernetes-tool/templates/categorisation_view.vue
@@ -10,13 +10,27 @@
                     {{ key.description }} <a v-if="key.learnMore" :href="key.learnMore">Learn more...</a>
                 </p>
                 <div v-for="item in allCats.get(key)" :key="item.fp" :ref="item.fp">
-                    <hr>
-                    <p>
-                        <a style="text-decoration: none;" @click="handleItem(key, item)">
-                            {{ item.fp }}
-                        </a>
-                    </p>
-                    <hr v-if="(showing[key.name] || {})[item.fp] === undefined ? true : showing[key.name][item.fp]">
+                    <hr style="margin-top: 0">
+                    <div class="columns is-gapless" style="margin: 0">
+                        <div class="column is-half">
+                            <p>
+                                <a style="text-decoration: none; color: black;" @click="handleItem(key, item)">
+                                    {{ item.fp }}
+                                </a>
+                            </p>
+                        </div>
+                        <div class="column is-half">
+                            <p style="text-align: right">
+                                <span v-if="(showing[key.name] || {})[item.fp] === undefined ? true : showing[key.name][item.fp]">
+                                    -
+                                </span>
+                                <span v-else>
+                                    +
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                    <hr v-if="(showing[key.name] || {})[item.fp] === undefined ? true : showing[key.name][item.fp]" style="margin-top: 0">
                     <SplitView v-if="(showing[key.name] || {})[item.fp] === undefined ? true : showing[key.name][item.fp]" :title="item.fp" :yaml="toBeRendered[item.fp]" :properties="parsed[item.fp]"></SplitView>
                 </div>
                 <hr v-if="index !== Array.from(allCats.keys()).length - 1">
