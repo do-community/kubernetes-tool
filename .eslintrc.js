d0ee57f1293fc12d57ac9b63eeb2ce2aac4c5ef2
Matt Cowley <me@mattcowley.co.uk>
2022-02-18 16:28:46 +0000

    Dependency updates (#55)

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports =  {
     }],
     "@typescript-eslint/indent": 0,
     "vue/no-unused-vars": 0,
+    "vue/multi-word-component-names": 0,
     "@typescript-eslint/no-object-literal-type-assertion": 0,
     "@typescript-eslint/no-empty-interface": 0,
     "@typescript-eslint/no-empty-function": 0,
